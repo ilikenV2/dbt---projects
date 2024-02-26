c2ba5cbc828f1c538eaa28e2ac8a478a5cba532e
Ilian Mouhoubi-Duhamel <ilian.mouhoubi-duhamel@siemens.com>
2024-02-26 17:00:57 +0000

    a

--- models/test_BigQuery/dim_customers.sql
+++ models/test_BigQuery/dim_customers.sql
@@ -1,22 +1,12 @@
 {{ config(materialized="table") }}
 
-with customers as (
-    select 
-        id as customer_id, 
-        first_name,
-        last_name
 
-    from dataset1.dbt_customer
+with customers as (
+    select * from {{ref ('stg_customers')}}
 ),
 
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dataset1.dbt_orders
+    select * from {{ref ('stg_orders')}}
 ),
 
 customer_orders as (
--- models/test_BigQuery/stg_orders.sql
+++ models/test_BigQuery/stg_orders.sql
@@ -1,6 +1,8 @@
+{{ config(materialized="table") }}
+
 with orders as (
     select
-        id as order_idc,
+        id as order_id,
         user_id as customer_id,
         order_date,
         status
